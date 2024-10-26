ac1f8d05fc37b3c25b8dc5e9693e01907c82cf12
Holson Mpangala <holsonm74@gmail.com>
2024-10-26 12:28:56 +0100

    Update Main Plugin

--- plugins/main.js
+++ plugins/main.js
@@ -1,9 +1,15 @@
 export default defineNuxtPlugin(() => {
-    if (process.client) {
+    if (process.client && !window.isScriptsLoaded) {
+      // Assurez-vous que les scripts sont chargés une seule fois
       const script = document.createElement('script');
       script.src = '/assets/js/main.js';
       script.async = true;
       document.head.appendChild(script);
+  
+      // Marquer que les scripts sont déjà chargés
+      window.isScriptsLoaded = true;
+  
+
     }
-  });
+});
   
