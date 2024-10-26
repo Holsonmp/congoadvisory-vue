2cc2a9805ef8e26cf20bd6a709b03368fa341360
Holson Mpangala <holsonm74@gmail.com>
2024-10-26 09:54:14 +0100

    Add: scrollreveal npm install scrollreveal --save

--- plugins/scrollreveal.js
+++ plugins/scrollreveal.js
@@ -1,20 +1,8 @@
+// plugins/scrollReveal.js
+import ScrollReveal from 'scrollreveal';
+
 export default defineNuxtPlugin(() => {
-	if (process.client) {
-	  const script = document.createElement('script');
-	  script.src = 'https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js';
-	  script.async = true;
-	  script.onload = () => {
-		// Initialiser ScrollReveal après le chargement du script
-		ScrollReveal({
-		  scale: 0.85,
-		  duration: 1000,
-		});
-  
-		// Manipuler la classe 'no-js' pour ajouter 'js'
-		document.documentElement.classList.remove('no-js');
-		document.documentElement.classList.add('js');
-	  };
-	  document.head.appendChild(script);
-	}
-  });
-  
+  if (process.client) {
+    window.ScrollReveal = ScrollReveal; // Rends ScrollReveal global
+  }
+});
