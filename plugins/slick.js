1e4f39e9d19e0c914b9872966905c73bf1d99ce0
Holson Mpangala <holsonm74@gmail.com>
2024-10-26 10:38:52 +0100

    import 'slick-carousel';

--- plugins/slick.js
+++ plugins/slick.js
@@ -1,9 +1,20 @@
+// plugins/slick.js
+import 'slick-carousel'; // Importer le package Slick
+
 export default defineNuxtPlugin(() => {
-    if (process.client) {
-      const script = document.createElement('script');
-      script.src = '/assets/js/slick.min.js';
-      script.async = true;
-      document.head.appendChild(script);
-    }
+  if (process.client) {
+    // Rendre jQuery et Slick disponibles globalement
+    window.jQuery = require('jquery');
+    require('slick-carousel'); // S'assurer que Slick est chargé
+
+    // Initialisation de Slick si nécessaire
+    window.jQuery.fn.slick && window.jQuery('.global-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // Ajoutez d'autres options ici si nécessaire
+    });
+  }
 });
-  
